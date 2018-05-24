773764b2fb1c95a157ebd276d34cb89b88b586c8
amadzia <agnieszkamadzia@onet.pl>
2018-05-24 14:57:05 +0200

    Iteration in Thymeleaf

--- src/main/java/com/example/thymeleafdemo/controllers/IndexController.java
+++ src/main/java/com/example/thymeleafdemo/controllers/IndexController.java
@@ -1,13 +1,26 @@
 package com.example.thymeleafdemo.controllers;
 
+import com.example.thymeleafdemo.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+
+        model.addAttribute("products", productService.listProducts());
+
         return "index";
     }
 }
