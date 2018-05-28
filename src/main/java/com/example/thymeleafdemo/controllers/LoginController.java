4c38ddc0aed757046fa5f1857c207a304553b272
amadzia <agnieszkamadzia@onet.pl>
2018-05-28 18:03:29 +0200

    Prototype of login form, with validations

---
+++ src/main/java/com/example/thymeleafdemo/controllers/LoginController.java
@@ -0,0 +1,32 @@
+package com.example.thymeleafdemo.controllers;
+
+import com.example.thymeleafdemo.commands.LoginCommand;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String showLoginForm(Model model) {
+
+        model.addAttribute("loginCommand", new LoginCommand());
+
+        return "loginform";
+    }
+
+    @RequestMapping(value = "/dologin", method = RequestMethod.POST)
+    public String doLogin(@Valid LoginCommand loginCommand, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "loginform";
+        }
+
+        return "redirect:index";
+    }
+}
