8662342912d3174227e81158f31783f8801e75fe
amadzia <agnieszkamadzia@onet.pl>
2018-05-29 17:20:15 +0200

    Basic Spring Security configuration added

--- src/main/java/com/example/thymeleafdemo/controllers/LoginController.java
+++ src/main/java/com/example/thymeleafdemo/controllers/LoginController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
 
@@ -20,7 +19,13 @@ public class LoginController {
         return "loginform";
     }
 
-    @RequestMapping(value = "/dologin", method = RequestMethod.POST)
+    @RequestMapping("logout-success")
+    public String yourLoggedOut() {
+
+        return "logout-success";
+    }
+
+    //  @RequestMapping(value = "/dologin", method = RequestMethod.POST)
     public String doLogin(@Valid LoginCommand loginCommand, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
